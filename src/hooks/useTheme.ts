a1e47bae68f907ab45a21887360c60a8b6b145c3
Seema Ullal <seema.ullal@gusto.com>
2021-01-31 22:48:19 -0800

    wrap toggleTheme in useCallback

--- src/hooks/useTheme.ts
+++ src/hooks/useTheme.ts
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useCallback } from 'react';
 import ThemeContext from '../contexts/theme_context';
 
 export function useTheme() {
@@ -8,8 +8,10 @@ export function useTheme() {
       'theme must be set using ThemeContext.Provider  before using'
     );
   }
-  const toggleTheme = () =>
-    setTheme((theme) => (theme === 'light' ? 'dark' : 'light'));
+  const toggleTheme = useCallback(
+    () => setTheme((theme) => (theme === 'light' ? 'dark' : 'light')),
+    []
+  );
 
   return { toggleTheme, isLightMode: theme === 'light' };
 }
