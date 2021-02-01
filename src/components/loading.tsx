b637f36f66d58c1e01e99e3bb15be40999f937bc
Seema Ullal <seema.ullal@gusto.com>
2021-02-01 02:39:36 -0800

    types for loading component

--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -1,3 +1,34 @@
-export default function Loading() {
-  return <div>Loading...</div>;
+import { useState, useEffect, CSSProperties } from 'react';
+
+const styles = {
+  content: {
+    fontSize: '35px',
+    position: 'absolute',
+    left: '0',
+    right: '0',
+    marginTop: '20px',
+    textAlign: 'center',
+  } as CSSProperties,
+};
+
+function Loading({
+  text = 'Loading',
+  speed = 300,
+}: {
+  text?: string;
+  speed?: number;
+}) {
+  const [content, setContent] = useState<string>(text);
+  useEffect(() => {
+    const intervalId = window.setInterval(() => {
+      content === text + '...'
+        ? setContent(text)
+        : setContent(currentContent => content + '.');
+    }, speed);
+    return () => {
+      window.clearInterval(intervalId);
+    };
+  }, [speed, text]);
+
+  return <p style={styles.content}>{content}</p>;
 }
