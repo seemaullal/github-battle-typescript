ae60416b8802accbfdaf8b345d8bf091b44d6b7f
Seema Ullal <seema.ullal@gusto.com>
2021-01-31 17:10:04 -0800

    remove theme context and just use a hook

--- src/contexts/themeContext.ts
+++
@@ -1,8 +0,0 @@
-import * as React from "react";
-export type themeType = "light" | "dark";
-
-const ThemeContext = React.createContext<themeType>("light");
-
-export default ThemeContext;
-export const ThemeConsumer = ThemeContext.Consumer;
-export const ThemeProvider = ThemeContext.Provider;
--- src/hooks/useTheme.ts
+++ src/hooks/useTheme.ts
@@ -1,15 +1,10 @@
-import { useContext, useState } from "react";
-import ThemeContext, {
-  themeType,
-  ThemeProvider,
-  ThemeConsumer,
-} from "../contexts/themeContext";
+import { useState } from 'react';
 
 export function useTheme() {
-  const themeContext = useContext<themeType>(ThemeContext);
-  const [theme, setTheme] = useState<themeType>("light");
+  type themeType = 'light' | 'dark';
+  const [theme, setTheme] = useState<themeType>('light');
   const toggleTheme = () =>
-    setTheme((theme) => (theme === "light" ? "dark" : "light"));
+    setTheme((theme) => (theme === 'light' ? 'dark' : 'light'));
 
   return { theme, toggleTheme };
 }
