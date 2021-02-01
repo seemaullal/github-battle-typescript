f06982181cd0b7da87ee43410c90b815209b608b
Seema Ullal <seema.ullal@gusto.com>
2021-02-01 01:53:58 -0800

    convert tooltip and useHover

---
+++ src/hooks/useHover.ts
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default function useHover(): [
+  boolean,
+  { onMouseOut: () => void; onMouseOver: () => void }
+] {
+  const [hovering, setHovering] = React.useState<boolean>(false);
+
+  const onMouseOver = () => setHovering(true);
+  const onMouseOut = () => setHovering(false);
+
+  return [
+    hovering,
+    {
+      onMouseOut,
+      onMouseOver,
+    },
+  ];
+}
