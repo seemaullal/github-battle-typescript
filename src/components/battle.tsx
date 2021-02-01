b5348bda34dff10ff526fc6ec99e52a67f446e56
Seema Ullal <seema.ullal@gusto.com>
2021-02-01 02:23:11 -0800

    battle component in ts

--- src/components/battle.tsx
+++ src/components/battle.tsx
@@ -1,3 +1,165 @@
+import { useState, FormEvent, MouseEvent } from 'react';
+import {
+  FaUserFriends,
+  FaFighterJet,
+  FaTrophy,
+  FaTimesCircle,
+} from 'react-icons/fa';
+import { useTheme } from '../hooks/useTheme';
+import { Link } from 'react-router-dom';
+
+function Instructions() {
+  const { theme } = useTheme();
+  return (
+    <div className="instructions-container">
+      <h1 className="center-text header-lg">Instructions</h1>
+      <ol className="container-sm grid center-text battle-instructions">
+        <li>
+          <h3 className="header-sm">Enter two Github users</h3>
+          <FaUserFriends
+            className={`bg-${theme}`}
+            color="rgb(255, 191, 116)"
+            size={140}
+          />
+        </li>
+        <li>
+          <h3 className="header-sm">Battle</h3>
+          <FaFighterJet className={`bg-${theme}`} color="#727272" size={140} />
+        </li>
+        <li>
+          <h3 className="header-sm">See the winners</h3>
+          <FaTrophy
+            className={`bg-${theme}`}
+            color="rgb(255, 215, 0)"
+            size={140}
+          />
+        </li>
+      </ol>
+    </div>
+  );
+}
+
+interface PlayerInputProps {
+  onSubmit: (username: string) => void;
+  label: string;
+}
+
+function PlayerInput({ onSubmit, label }: PlayerInputProps) {
+  const [username, setUsername] = useState<string>('');
+  const { theme } = useTheme();
+
+  function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+    onSubmit(username);
+  }
+
+  return (
+    <form className="column player" onSubmit={handleSubmit}>
+      <label htmlFor="username" className="player-label">
+        {label}
+      </label>
+      <div className="row player-inputs">
+        <input
+          type="text"
+          id="username"
+          className={`input-${theme}`}
+          placeholder="github username"
+          autoComplete="off"
+          value={username}
+          onChange={event => setUsername(event.target.value)}
+        />
+        <button
+          className={`btn ${theme === 'dark' ? 'light-btn' : 'dark-btn'}`}
+          type="submit"
+          disabled={!username}
+        >
+          Submit
+        </button>
+      </div>
+    </form>
+  );
+}
+
+interface PlayerPreviewProps {
+  username: string;
+  onReset: () => void;
+  label: string;
+}
+
+function PlayerPreview({ username, onReset, label }: PlayerPreviewProps) {
+  const { theme } = useTheme();
+  return (
+    <div className="column player">
+      <h3 className="player-label">{label}</h3>
+      <div className={`row bg-${theme}`}>
+        <div className="player-info">
+          <img
+            className="avatar-small"
+            src={`https://github.com/${username}.png?size=200`}
+            alt={`Avatar for ${username}`}
+          />
+          <a href={`https://github.com/${username}`} className="link">
+            {username}
+          </a>
+        </div>
+        <button className="btn-clear flex-center" onClick={onReset}>
+          <FaTimesCircle color="rgb(194, 57, 42)" size={26} />
+        </button>
+      </div>
+    </div>
+  );
+}
+
 export default function Battle() {
-  return <div>Battle</div>;
+  const [playerOne, setPlayerOne] = useState<string | null>(null);
+  const [playerTwo, setPlayerTwo] = useState<string | null>(null);
+
+  return (
+    <>
+      <Instructions />
+
+      <div className="players-container">
+        <h1 className="center-text header-lg">Players</h1>
+        <div className="row space-around">
+          {playerOne === null ? (
+            <PlayerInput
+              label="Player One"
+              onSubmit={player => setPlayerOne(player)}
+            />
+          ) : (
+            <PlayerPreview
+              username={playerOne}
+              label="Player One"
+              onReset={() => setPlayerOne(null)}
+            />
+          )}
+
+          {playerTwo === null ? (
+            <PlayerInput
+              label="Player Two"
+              onSubmit={player => setPlayerTwo(player)}
+            />
+          ) : (
+            <PlayerPreview
+              username={playerTwo}
+              label="Player Two"
+              onReset={() => setPlayerTwo(null)}
+            />
+          )}
+        </div>
+
+        {playerOne && playerTwo && (
+          <Link
+            className="btn dark-btn btn-space"
+            to={{
+              pathname: '/battle/results',
+              search: `?playerOne=${playerOne}&playerTwo=${playerTwo}`,
+            }}
+          >
+            Battle
+          </Link>
+        )}
+      </div>
+    </>
+  );
 }
