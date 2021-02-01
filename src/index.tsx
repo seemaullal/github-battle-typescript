7ee0a6f9ce0deaf60ff063e205fce31e88ba6f66
Seema Ullal <seema.ullal@gusto.com>
2021-02-01 00:53:14 -0800

    set up routes, skeleton components

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,14 @@
-import { useState } from 'react';
+import { useState, Suspense, lazy } from 'react';
 import * as ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import ThemeContext, { themeType } from './contexts/theme_context';
 import Navigation from './components/navigation';
+import Loading from './components/loading';
 import './styles.scss';
+
+const Popular = lazy(() => import('./components/popular'));
+const Battle = lazy(() => import('./components/battle'));
+const Results = lazy(() => import('./components/results'));
 
 function App() {
   const [theme, setTheme] = useState<themeType>('light');
@@ -13,14 +18,17 @@ function App() {
         <div className={theme}>
           <div className="container">
             <Navigation />
-            <Switch>
-              <Route exact path="/">
-                Hello
-              </Route>
-              <Route exact path="/battle">
-                Battle
-              </Route>
-            </Switch>
+            <Suspense fallback={<Loading />}>
+              <Switch>
+                <Route exact path="/" component={Popular} />
+                <Route exact path="/battle" component={Battle} />
+                <Route exact path="/battle/results" component={Results} />
+                {/* redirect back to homepage if no matching route */}
+                <Route>
+                  <Redirect to="/" />
+                </Route>
+              </Switch>
+            </Suspense>
           </div>
         </div>
       </ThemeContext.Provider>
