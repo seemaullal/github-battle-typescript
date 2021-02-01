df6f4fbe3765c62b031ff2378a66bebf7028a94f
Seema Ullal <seema.ullal@gusto.com>
2021-02-01 02:27:22 -0800

    card component

---
+++ src/components/card.tsx
@@ -0,0 +1,36 @@
+import { ReactNode } from 'react';
+import PropTypes from 'prop-types';
+import { useTheme } from '../hooks/useTheme';
+
+interface CardProps {
+  header: string;
+  subheader?: string;
+  avatar: string;
+  href: string;
+  name: string;
+  children: ReactNode;
+}
+export default function Card({
+  header,
+  subheader,
+  avatar,
+  href,
+  name,
+  children,
+}: CardProps) {
+  const { theme } = useTheme();
+
+  return (
+    <div className={`card bg-${theme}`}>
+      <h4 className="header-lg center-text">{header}</h4>
+      <img className="avatar" src={avatar} alt={`Avatar for ${name}`} />
+      {subheader && <h4 className="center-text">{subheader}</h4>}
+      <h2 className="center-text">
+        <a className="link" href={href}>
+          {name}
+        </a>
+      </h2>
+      {children}
+    </div>
+  );
+}
